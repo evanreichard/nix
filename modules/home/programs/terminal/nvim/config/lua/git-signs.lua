d6c908dc70288fed06837e2dc3397a8210287a81
Evan Reichard <evan@reichard.io>
2025-06-09 13:22:03 -0400

    git signs tweak

--- modules/home/programs/terminal/nvim/config/lua/git-signs.lua
+++ modules/home/programs/terminal/nvim/config/lua/git-signs.lua
@@ -1,16 +1,18 @@
-require('gitsigns').setup {
-    on_attach = function(bufnr)
-        local gitsigns = require('gitsigns')
+require("gitsigns").setup({
+	current_line_blame = true,
+	current_line_blame_opts = { delay = 0 },
+	on_attach = function(bufnr)
+		local gitsigns = require("gitsigns")
 
-        local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-        end
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
 
-        map('n', '<leader>gb', gitsigns.toggle_current_line_blame)
-        map('n', '<leader>gB', function()
-            gitsigns.blame_line {full = true}
-        end)
-    end
-}
+		map("n", "<leader>gb", gitsigns.toggle_current_line_blame)
+		map("n", "<leader>gB", function()
+			gitsigns.blame_line({ full = true })
+		end)
+	end,
+})
