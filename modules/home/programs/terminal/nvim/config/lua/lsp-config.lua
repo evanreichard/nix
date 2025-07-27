6b42b3cc2269aae0e97b3656be45a21ccb6023b0
Evan Reichard <evan@reichard.io>
2025-07-27 18:22:11 -0400

    vim deprecated functions

--- modules/home/programs/terminal/nvim/config/lua/lsp-config.lua
+++ modules/home/programs/terminal/nvim/config/lua/lsp-config.lua
@@ -23,7 +23,7 @@ local nvim_lsp = require("lspconfig")
 local on_attach = function(client, bufnr)
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-	if client.supports_method("textDocument/formatting") then
+	if client:supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			group = augroup,
@@ -229,7 +229,7 @@ none_ls.setup({
 		}),
 	},
 	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
+		if client:supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augroup,
