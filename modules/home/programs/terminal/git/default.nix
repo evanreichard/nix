3d55b6e6752a1ec235e4ee7bbc33395f09ee7ef8
Evan Reichard <evan@reichard.io>
2026-04-30 20:14:41 -0400

    chore: add lfs

--- modules/home/programs/terminal/git/default.nix
+++ modules/home/programs/terminal/git/default.nix
@@ -16,6 +16,7 @@ in
   config = mkIf cfg.enable {
     programs.git = {
       enable = true;
+      lfs.enable = true;
       settings = {
         aliases = {
           lg = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all -n 15";
