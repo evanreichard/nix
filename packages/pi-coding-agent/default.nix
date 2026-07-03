96bb6e0752c59a78ce2c8c9858ce80d037a1d8bf
Evan Reichard <evan@reichard.io>
2026-07-03 15:44:00 -0400

    build(pi-coding-agent): bump to 0.80.3

--- packages/pi-coding-agent/default.nix
+++ packages/pi-coding-agent/default.nix
@@ -18,16 +18,16 @@
 
 buildNpmPackage rec {
   pname = "pi-coding-agent";
-  version = "0.80.2";
+  version = "0.80.3";
 
   src = fetchFromGitHub {
     owner = "earendil-works";
     repo = "pi-mono";
     rev = "v${version}";
-    hash = "sha256-aKtgPc3rwHEp856jP3N7nImph0CSG+gsWq9OVci3hmE=";
+    hash = "sha256-wQTrWKsb2HCGwzSAFEk8NWSDpqxSY/lv1/R6ghcmbaA=";
   };
 
-  npmDepsHash = "sha256-1EGs8lX8XoAnRtS+pw4lBRm24U/vtVB2loVRmZyd4Z8=";
+  npmDepsHash = "sha256-geh8LH88OZybFXkR/jDeTdew6TNMdFM6jhCSYKn//dU=";
 
   nativeBuildInputs = [ pkg-config makeWrapper ];
 
