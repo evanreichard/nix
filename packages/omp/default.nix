0a9f3c7f4f1492a06dd0b26b7e523737c58420eb
Evan Reichard <evan@reichard.io>
2026-08-28 12:04:57 -0400

    chore(omp): refresh hashes for 18.0.7

--- packages/omp/default.nix
+++ packages/omp/default.nix
@@ -7,24 +7,24 @@
 
 let
   pname = "omp";
-  version = "18.0.4";
+  version = "18.0.7";
 
   sources = {
     aarch64-darwin = {
       asset = "omp-darwin-arm64";
-      hash = "sha256-1JMWOIe8+Pd7mZGrchn3dxK1sn3mVk969ygwZKyoSCQ=";
+      hash = "sha256-dS1GagwiFl+AEPQa6m/X2n6P10TYQfU9Yr+2228hj00=";
     };
     x86_64-darwin = {
       asset = "omp-darwin-x64";
-      hash = "sha256-8hd1L19XnSsiCBhGL8UVrTM+ikEPG7/NYweCW3BSYPU=";
+      hash = "sha256-65NRdacNnnBFdTFeXWz1Zqbdy+AtrmePUAHA/ey+M0w=";
     };
     aarch64-linux = {
       asset = "omp-linux-arm64";
-      hash = "sha256-8rfIoBloHt4xSsFlEAwcW1zUkAE5B1lI2oCcAEvsXOc=";
+      hash = "sha256-a/QSnvC/WVmSiHmyz5769ADslgiVQAXh6+gYPnZJDpA=";
     };
     x86_64-linux = {
       asset = "omp-linux-x64";
-      hash = "sha256-lOxC0X1xl1o4HiAzW7PABaf9fuwZsxk1jfbSLyjhazc=";
+      hash = "sha256-TiRorWl05qLt6mIdqCq8qMlexiqDVGMDgcNT3AjHdps=";
     };
   };
 
